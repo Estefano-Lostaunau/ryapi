4cc136927be3c2b610fbc33d2694ea63a3096991
Estefano-Lostaunau <u202211742@upc.edu.pe>
2025-01-09 23:46:16 -0500

    feat: add jwt-decode dependency and integrate JWT decoding in Auth component

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,14 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './app/App.tsx'
+import { GoogleOAuthProvider } from '@react-oauth/google'
+
+const CLIEND_ID = "182560334827-a483crbq7b6bm97cv4n1smbe7tnkijmo.apps.googleusercontent.com"
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
+  <GoogleOAuthProvider clientId={CLIEND_ID}>
     <App />
+  </GoogleOAuthProvider>
   </StrictMode>,
 )
